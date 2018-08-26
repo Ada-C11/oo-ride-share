316e6068b87e0b83dc60f661aebe6601a3f93906
CheezItMan <chris@adadevelopersacademy.org>
2018-08-26 12:12:42 -0700

    update spec on trip duration

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,23 +1,23 @@
 require_relative 'spec_helper'
 
 describe "Trip class" do
+  before do
+    start_time = Time.parse('2015-05-20T12:14:00+00:00')
+    end_time = start_time + 25 * 60 # 25 minutes
+    @trip_data = {
+      id: 8,
+      passenger: RideShare::User.new(id: 1,
+                                     name: "Ada",
+                                     phone: "412-432-7640"),
+      start_time: start_time,
+      end_time: end_time,
+      cost: 23.45,
+      rating: 3
+    }
+    @trip = RideShare::Trip.new(@trip_data)
+  end
 
   describe "initialize" do
-    before do
-      start_time = Time.parse('2015-05-20T12:14:00+00:00')
-      end_time = start_time + 25 * 60 # 25 minutes
-      @trip_data = {
-        id: 8,
-        passenger: RideShare::User.new(id: 1,
-                                       name: "Ada",
-                                       phone: "412-432-7640"),
-        start_time: start_time,
-        end_time: end_time,
-        cost: 23.45,
-        rating: 3
-      }
-      @trip = RideShare::Trip.new(@trip_data)
-    end
 
     it "is an instance of Trip" do
       expect(@trip).must_be_kind_of RideShare::Trip
@@ -40,5 +40,9 @@ describe "Trip class" do
         }.must_raise ArgumentError
       end
     end
+  end
+
+  it "can calculate the trip duration" do
+    expect(@trip.duration).must_equal 25 * 60
   end
 end
