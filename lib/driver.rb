9d15c35fd9e47e62da2718766570d0730321f08e
CheezItMan <chris@adadevelopersacademy.org>
2018-08-26 13:06:59 -0700

    initial driver with driven_trips and status

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,14 +1,17 @@
 
 module RideShare
   class Driver < User
-    attr_reader :vin
+    attr_reader :vin, :driven_trips
 
     def initialize(input)
       super(input)
 
       raise ArgumentError, "Vin must be 17 characters long" if input[:vin].length != 17
+      raise ArgumentError, "Status should be available or unavailable" unless [:AVAILABLE, :UNAVAILABLE].includes? input[:status]
 
       @vin = input[:vin]
+      @status = input[:status]
+      @driven_trips = []
 
     end
 
