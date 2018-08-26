1751be6a7e1da49e634f77b3cf4842f50f62bab2
CheezItMan <chris@adadevelopersacademy.org>
2018-08-26 13:25:10 -0700

    Update trip to make end_time optional

--- lib/trip.rb
+++ lib/trip.rb
@@ -14,10 +14,8 @@ module RideShare
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
-      [@start_time, @end_time].each do |time|
-        [:-, :to_i].each do |method|
-          raise ArgumentError, "Times must be subtractable and convertable to integers" unless time.respond_to? method
-        end
+      [:-, :to_i].each do |method|
+        raise ArgumentError, "Times must be subtractable and convertable to integers" unless @start_time.respond_to? method
       end
     end
 
