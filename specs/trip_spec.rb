b8ca98306134e733d896d1fd37aad6ed4b8e0da7
qqdipps <sdipp007@gmail.com>
2019-03-04 16:18:14 -0800

    Added test to check if start time is type of Time

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,23 +1,22 @@
-require_relative 'spec_helper'
+require_relative "spec_helper"
 
 describe "Trip class" do
+  before do
+    start_time = Time.parse("2015-05-20T12:14:00+00:00")
+    end_time = start_time + 25 * 60 # 25 minutes
+    @trip_data = {
+      id: 8,
+      passenger: RideShare::Passenger.new(id: 1,
+                                          name: "Ada",
+                                          phone_number: "412-432-7640"),
+      start_time: start_time.to_s,
+      end_time: end_time.to_s,
+      cost: 23.45,
+      rating: 3,
+    }
+    @trip = RideShare::Trip.new(@trip_data)
+  end
   describe "initialize" do
-    before do
-      start_time = Time.parse('2015-05-20T12:14:00+00:00')
-      end_time = start_time + 25 * 60 # 25 minutes
-      @trip_data = {
-        id: 8,
-        passenger: RideShare::Passenger.new(id: 1,
-                                            name: "Ada",
-                                            phone_number: "412-432-7640"),
-        start_time: start_time.to_s,
-        end_time: end_time.to_s,
-        cost: 23.45,
-        rating: 3
-      }
-      @trip = RideShare::Trip.new(@trip_data)
-    end
-
     it "is an instance of Trip" do
       expect(@trip).must_be_kind_of RideShare::Trip
     end
@@ -38,6 +37,12 @@ describe "Trip class" do
           RideShare::Trip.new(@trip_data)
         end.must_raise ArgumentError
       end
+    end
+  end
+
+  describe "Wave 1: Update time" do
+    it "start_time should be of Time class" do
+      expect(@trip.start_time).must_be_instance_of Time
     end
   end
 end
