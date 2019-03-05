587cea1329f6f528814affc0781d8af1fe6a39c4
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-03-04 17:14:11 -0800

    wrote tests for end_time and duration_in_seconds

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,9 +1,9 @@
-require_relative 'spec_helper'
+require_relative "spec_helper"
 
 describe "Trip class" do
   describe "initialize" do
     before do
-      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      start_time = Time.parse("2015-05-20T12:14:00+00:00")
       end_time = start_time + 25 * 60 # 25 minutes
       @trip_data = {
         id: 8,
@@ -13,9 +13,38 @@ describe "Trip class" do
         start_time: start_time.to_s,
         end_time: end_time.to_s,
         cost: 23.45,
-        rating: 3
+        rating: 3,
       }
       @trip = RideShare::Trip.new(@trip_data)
+    end
+
+    it "raises ArgumentError if end time is before start time" do
+      # start_time = Time.parse("2015-05-20T12:14:00+00:00")
+      # end_time = start_time - 25 * 60 # 25 minutes
+
+      # puts @trip.start_time
+
+      # total_trip_time = end_time - start_time
+      # expect (total_trip_time).must_raise ArgumentError
+
+      start_time = Time.parse("2015-05-20T12:14:00+00:00")
+      end_time = Time.parse("2010-05-20T12:14:00+00:00") #start_time - 25 * 60 # 25 minutes
+      @bad_trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1,
+                                            name: "Ada",
+                                            phone_number: "412-432-7640"),
+        start_time: start_time.to_s,
+        end_time: end_time.to_s,
+        cost: 23.45,
+        rating: 3,
+      }
+      # @bad_trip = RideShare::Trip.new(@bad_trip_data)
+
+      expect {
+        RideShare::Trip.new(@bad_trip_data)
+      }.must_raise ArgumentError
+      # expect(RideShare::Trip.new(@trip_data)).must_raise ArgumentError
     end
 
     it "is an instance of Trip" do
@@ -38,6 +67,32 @@ describe "Trip class" do
           RideShare::Trip.new(@trip_data)
         end.must_raise ArgumentError
       end
+    end
+  end
+  # this is outside "initialize" class!!!
+  describe "duration_in_seconds" do
+    it "calculates duration of trip in seconds" do
+      # Arrange
+      start_time = Time.parse("2015-05-20T12:14:00+00:00")
+      end_time = start_time + 25 * 60 # 25 minutes
+
+      trip_with_duration_of_1500 = RideShare::Trip.new({
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1,
+                                            name: "Ada",
+                                            phone_number: "412-432-7640"),
+        start_time: start_time.to_s,
+        end_time: end_time.to_s,
+        cost: 23.45,
+        rating: 3,
+      })
+
+      # Act
+      trip = trip_with_duration_of_1500.duration_in_seconds
+
+      # Assert
+      # expect ___whatever our ACT returns___ .must_equal ___whatever our hard-coded answer is___
+      expect (trip).must_equal 1500
     end
   end
 end
