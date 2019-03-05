493d1a6f2aff37719f8f7468c27401fae68af3ce
Jansen <v.jansen.martin@gmail.com>
2019-03-05 12:32:06 -0800

    Add test for invalid times

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,9 +1,9 @@
-require_relative 'spec_helper'
+require_relative "spec_helper"
 
 describe "Trip class" do
   describe "initialize" do
     before do
-      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      start_time = Time.parse("2015-05-20T12:14:00+00:00")
       end_time = start_time + 25 * 60 # 25 minutes
       @trip_data = {
         id: 8,
@@ -13,7 +13,7 @@ describe "Trip class" do
         start_time: start_time.to_s,
         end_time: end_time.to_s,
         cost: 23.45,
-        rating: 3
+        rating: 3,
       }
       @trip = RideShare::Trip.new(@trip_data)
     end
@@ -38,6 +38,15 @@ describe "Trip class" do
           RideShare::Trip.new(@trip_data)
         end.must_raise ArgumentError
       end
+    end
+
+    it "raises an error for an invalid time" do
+      @trip_data[:start_time] = "2018-12-27 03:38:08 -0800"
+      @trip_data[:end_time] = "2018-12-27 02:39:05 -0800"
+
+      expect do
+        RideShare::Trip.new(@trip_data)
+      end.must_raise ArgumentError
     end
   end
 end
