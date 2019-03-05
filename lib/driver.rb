f201956899e76789cf3f5a6a37f7cc5ca98cbbc2
Riyo <riyo.perry@gmail.com>
2019-03-04 16:46:27 -0800

    added driver class

---
+++ lib/driver.rb
@@ -0,0 +1,35 @@
+require_relative "csv_record"
+
+module RideShare
+  class Driver < CsvRecord
+    attr_reader :id, :name, :vin, :status, :trips
+
+    def initialize(id:, name:, vin:, status: :AVAILABLE, trips: [])
+      super(id)
+      @name = name
+      if vin.length != 17
+        raise ArgumentError, "Vin length is too short"
+      else
+        @vin = vin
+      end
+      unless [:UNAVAILABLE, :AVAILABLE].include?(status)
+        raise ArgumentError, "Invalid status"
+      else
+        @status = status
+      end
+      @trips = trips
+    end
+
+    private
+
+    binding.pry
+    def self.from_csv(record)
+      return new(
+               id: record[:id],
+               name: record[:name],
+               vin: record[:vin],
+               status: record[:status],
+             )
+    end
+  end
+end
