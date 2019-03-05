916ed45c30e72b4c6cd5300bfe14d78d2082c8cb
Grace Shea <grace.m.shea@gmail.com>
2019-03-05 14:03:09 -0800

    first draft of net_expenditures method, still need to test

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,4 +1,4 @@
-require_relative 'csv_record'
+require_relative "csv_record"
 
 # Parent CsvRecord to Passenger class
 module RideShare
@@ -17,14 +17,29 @@ module RideShare
       @trips << trip
     end
 
+    #will return the total amount of money
+    # that passenger has spent on their trips
+    def net_expenditures
+      total = 0
+      @trips each do |trip|
+        net_cost = trip.cost 
+      end
+      return net_cost
+    end
+
+    # will return the total amount of time
+    # that passenger has spent on their trips
+    def total_time_spent
+    end
+
     private
 
     def self.from_csv(record)
       return new(
-        id: record[:id],
-        name: record[:name],
-        phone_number: record[:phone_num]
-      )
+               id: record[:id],
+               name: record[:name],
+               phone_number: record[:phone_num],
+             )
     end
   end
 end
