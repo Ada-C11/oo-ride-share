9250d51f71a402f1ace1ccf71cd71e528a64cf86
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-04 16:47:33 -0800

    added test for ArgumentError for invalid end time

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,9 +1,9 @@
-require_relative 'spec_helper'
+require_relative "spec_helper"
 
 describe "Trip class" do
   describe "initialize" do
     before do
-      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      start_time = Time.parse("2015-05-20T12:14:00+00:00")
       end_time = start_time + 25 * 60 # 25 minutes
       @trip_data = {
         id: 8,
@@ -13,7 +13,7 @@ describe "Trip class" do
         start_time: start_time.to_s,
         end_time: end_time.to_s,
         cost: 23.45,
-        rating: 3
+        rating: 3,
       }
       @trip = RideShare::Trip.new(@trip_data)
     end
@@ -29,6 +29,27 @@ describe "Trip class" do
     it "stores an instance of driver" do
       skip # Unskip after wave 2
       expect(@trip.driver).must_be_kind_of RideShare::Driver
+    end
+
+    it "raises an error for an invalid end time" do
+      start_time = Time.parse("2019-01-09 08:48:50 -0800")
+      end_time = Time.parse("2019-01-09 07:48:50 -0800")
+
+      @trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1,
+                                            name: "Ada",
+                                            phone_number: "412-432-7640"),
+        start_time: start_time.to_s,
+        end_time: end_time.to_s,
+        cost: 23.45,
+        rating: 3,
+      }
+      # @trip = RideShare::Trip.new(@trip_data)
+
+      expect {
+        RideShare::Trip.new(@trip_data)
+      }.must_raise ArgumentError
     end
 
     it "raises an error for an invalid rating" do
