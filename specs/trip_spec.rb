ec582f4ad0f4fae527cfa0f89849e0d0b3244203
Hana <hanalways@gmail.com>
2019-03-04 17:06:59 -0800

    finish trip duration method

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,23 +1,23 @@
 require_relative "spec_helper"
+require "pry"
 
 describe "Trip class" do
+  before do
+    start_time = Time.parse("2015-05-20T12:14:00+00:00")
+    end_time = start_time + 25 * 60 # 25 minutes
+    @trip_data = {
+      id: 8,
+      passenger: RideShare::Passenger.new(id: 1,
+                                          name: "Ada",
+                                          phone_number: "412-432-7640"),
+      start_time: start_time.to_s,
+      end_time: end_time.to_s,
+      cost: 23.45,
+      rating: 3,
+    }
+    @trip = RideShare::Trip.new(@trip_data)
+  end
   describe "initialize" do
-    before do
-      start_time = Time.parse("2015-05-20T12:14:00+00:00")
-      end_time = start_time + 25 * 60 # 25 minutes
-      @trip_data = {
-        id: 8,
-        passenger: RideShare::Passenger.new(id: 1,
-                                            name: "Ada",
-                                            phone_number: "412-432-7640"),
-        start_time: start_time.to_s,
-        end_time: end_time.to_s,
-        cost: 23.45,
-        rating: 3,
-      }
-      @trip = RideShare::Trip.new(@trip_data)
-    end
-
     it "is an instance of Trip" do
       expect(@trip).must_be_kind_of RideShare::Trip
     end
@@ -52,14 +52,7 @@ describe "Trip class" do
 
   describe "duration" do
     it "calculates duration of trip in seconds" do
-      @trip_data[:start_time] = "2015-04-20T12:14:00+00:00"
-      @trip_data[:end_time] = "2015-04-20T12:14:30+00:00"
-
-      new_trip = RideShare::Trip.new(@trip_data)
-
-      expect do
-        new_trip.duration
-      end.must_equal 30
+      expect(@trip.duration).must_equal 1500
     end
   end
 end
