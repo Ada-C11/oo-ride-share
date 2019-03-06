8606dbe16729d768954527c15f4c2b7ca95675cc
Nara <naraa_520@yahoo.com>
2019-03-05 16:01:15 -0800

    Updated trip_spec file with driver data.

--- lib/trip.rb
+++ lib/trip.rb
@@ -5,9 +5,10 @@ require_relative 'csv_record'
 
 module RideShare
   class Trip < CsvRecord
-    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
+    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating, :driver_id, :driver
     def initialize(id:,
       passenger: nil, passenger_id: nil,
+      driver: nil, driver_id: nil,
       start_time:, end_time:, cost: nil, rating:)
       super(id)
 
@@ -20,6 +21,17 @@ module RideShare
 
       else
         raise ArgumentError, 'Passenger or passenger_id is required'
+      end
+
+      if driver
+        @driver = driver
+        @driver_id = driver.id
+
+      elsif driver_id
+        @driver_id = driver_id
+      
+      else
+         raise ArgumentError, 'Driver or driver_id is required'
       end
 
       @start_time = Time.parse(start_time)
