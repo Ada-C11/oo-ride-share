5d81b89be5a3253600013cb74f7a730b477fbfcf
MyriamWD <myriam.waldend@gmail.com>
2019-03-05 18:18:39 -0800

    Uncommented instance of driver test in trip_spec.rb

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -33,7 +33,7 @@ describe "Trip class" do
     end
 
     it "stores an instance of driver" do
-      skip # Unskip after wave 2
+      # Unskip after wave 2
       expect(@trip.driver).must_be_kind_of RideShare::Driver
     end
 
