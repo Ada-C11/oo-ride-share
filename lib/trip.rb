f2b87898edcdee35b9aac13131e1d47c80d83297
Maria Wissler <wmcarmelina@gmail.com>
2019-03-05 18:27:04 -0800

    Add driver and driver_id attributes, add conditional to validate driver

--- lib/trip.rb
+++ lib/trip.rb
@@ -5,13 +5,15 @@ require_relative "csv_record"
 
 module RideShare
   class Trip < CsvRecord
-    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
+    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating, :driver_id, :driver 
 
     def initialize(id:,
                    passenger: nil, passenger_id: nil,
-                   start_time:, end_time:, cost: nil, rating:)
+                   start_time:, end_time:, cost: nil, rating:, driver_id: nil, driver: nil)
       super(id)
-
+      @driver_id = driver_id
+      @driver = driver
+      
       if passenger
         @passenger = passenger
         @passenger_id = passenger.id
@@ -19,6 +21,15 @@ module RideShare
         @passenger_id = passenger_id
       else
         raise ArgumentError, "Passenger or passenger_id is required"
+      end
+
+      if driver
+        @driver = driver
+        @driver_id = driver.id
+      elsif driver_id
+        @driver_id = driver_id
+      else
+        raise ArgumentError, "Driver or driver_id is required"
       end
 
       start_time = Time.parse(start_time)
