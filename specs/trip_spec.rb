736bba4bdcd6c3259bd4553a8062907261bb6fc3
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-03-05 20:32:24 -0800

    Adds trip duration test in trip_spec.rb

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -64,12 +64,36 @@ describe "Trip class" do
         # test_trip_1.start_time = existing_end_time + 25 * 60
       }.must_raise ArgumentError
     end
-
+    # TEST 6
     it "raises an error if start time is the same as end time" do
       @trip_data[:end_time] = @trip_data[:start_time]
       expect do
         RideShare::Trip.new(@trip_data)
       end.must_raise ArgumentError
+    end
+  end
+  # TEST 7
+  describe "duration method" do    
+    it "returns an integer for trip duration" do
+      # start_time = Time.parse(@trip_data[:start_time])
+      # end_time = Time.parse(@trip_data[:end_time])
+      start_time = Time.parse("2015-05-20T12:14:00+00:00")
+      end_time = start_time + 25 * 60 # 25 minutes
+      @trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1,
+                                            name: "Ada",
+                                            phone_number: "412-432-7640"),
+        start_time: start_time.to_s,
+        end_time: end_time.to_s,
+        cost: 23.45,
+        rating: 3,
+      }
+      # test_trip = RideShare::Trip.new(@trip_data)
+      start_time = Time.parse(@trip_data[:start_time])
+      end_time = Time.parse(@trip_data[:end_time])
+      # expect(end_time - start_time).must_equal 15 # fails
+      expect(end_time - start_time).must_equal 1500 # Passes
     end
   end
 end
