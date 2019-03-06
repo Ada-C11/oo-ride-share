f1c96edda3a3c2b6ce1383a8d4f95107e5284efc
Margaret Finnan <msfinnan@gmail.com>
2019-03-06 15:20:52 -0800

    Added average_rating method to Driver Class and turned more tests on

--- lib/driver.rb
+++ lib/driver.rb
@@ -21,12 +21,26 @@ module RideShare
         @vin = vin
       end
 
-      raise ArgumentError, "Driver must have a status of AVAILABLE or UNAVAILABLE" if status != ( :AVAILABLE || :UNAVAILABLE ) && status.nil?
-
+      raise ArgumentError, "Driver must have a status of AVAILABLE or UNAVAILABLE" if status != (:AVAILABLE || :UNAVAILABLE) && status.nil?
     end
+
     def add_trip(trip)
       @trips << trip
     end
+
+    def average_rating
+      @ratings_array = []
+      if @trips.length > 0
+        @trips.each do |trip|
+          @ratings_array << trip.rating
+        end
+        average_rating = @ratings_array.sum / @ratings_array.length
+        return average_rating.to_f
+      else
+        return 0
+      end
+    end
+
     private
 
     def self.from_csv(record)
