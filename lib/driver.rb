3b8d72555c223bd507eb34d625c7163a3069884a
MyriamWD <myriam.waldend@gmail.com>
2019-03-06 15:01:12 -0800

    added average_method to Driver class

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,30 +1,39 @@
 require_relative "csv_record"
-# require "csv"
 
 module RideShare
   class Driver < CsvRecord
     attr_reader :name, :vin, :status, :trips
 
-    def initialize(id:, name:, vin:, status: nil, trips: nil)
+    def initialize(id:, name:, vin:, status: :AVAILABLE, trips: nil)
       super(id)
       @name = name
 
       unless vin.length == 17
         raise ArgumentError, "VIN must be 17 characters long!"
       end
-
-      unless ["AVAILABLE", "UNAVAILABLE"].include?(status)
-        raise ArgumentError, "Status must be AVAILABLE or UNAVAILABLE! Got : #{status}"
+      @status = status
+      unless [:AVAILABLE, :UNAVAILABLE].include?(status)
+        raise ArgumentError, "Status must be AVAILABLE or UNAVAILABLE! Got->  #{status.class}"
       end
 
       @vin = vin
-      @status = status
+
       @trips = trips || []
     end
 
-    def connect(passenger)
-      @passenger = passenger
-      passenger.add_trip(self)
+    def add_trip(trip)
+      @trips << trip
+    end
+
+    def average_rating(driver)
+      rating = []
+      @trips.each do |trip|
+        if trip.driver_id == driver.id
+          rating << driver.id.rating
+        end
+      end
+      average_rating = rating.reduce(:+) / rating.length.to_f
+      return average_rating
     end
 
     private
@@ -34,7 +43,7 @@ module RideShare
                id: record[:id],
                name: record[:name],
                vin: record[:vin],
-               status: record[:status],
+               status: (record[:status]).to_sym,
                trips: record[:trips],
              )
     end
