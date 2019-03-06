ed62fce3a293082f2ba4e2aef41c3bbdb25965dd
Mello-Cello <celloelle@gmail.com>
2019-03-06 15:56:03 -0800

    Wrote tests to calculate totals for passenger trip time and cost of all trips by this passenger. Tests are failing as desired.

--- specs/passenger_spec.rb
+++ specs/passenger_spec.rb
@@ -1,7 +1,6 @@
-require_relative 'spec_helper'
+require_relative "spec_helper"
 
 describe "Passenger class" do
-
   describe "Passenger instantiation" do
     before do
       @passenger = RideShare::Passenger.new(id: 1, name: "Smithy", phone_number: "353-533-5334")
@@ -34,7 +33,6 @@ describe "Passenger class" do
     end
   end
 
-
   describe "trips property" do
     before do
       # TODO: you'll need to add a driver at some point here.
@@ -42,15 +40,15 @@ describe "Passenger class" do
         id: 9,
         name: "Merl Glover III",
         phone_number: "1-602-620-2330 x3723",
-        trips: []
-        )
+        trips: [],
+      )
       trip = RideShare::Trip.new(
         id: 8,
         passenger: @passenger,
         start_time: "2016-08-08",
         end_time: "2016-08-09",
-        rating: 5
-        )
+        rating: 5,
+      )
 
       @passenger.add_trip(trip)
     end
@@ -65,6 +63,78 @@ describe "Passenger class" do
       @passenger.trips.each do |trip|
         expect(trip.passenger.id).must_equal 9
       end
+    end
+  end
+
+  describe "Passenger totals" do
+    before do
+      @passenger = RideShare::Passenger.new(
+        id: 9,
+        name: "Merl Glover III",
+        phone_number: "1-602-620-2330 x3723",
+        trips: [],
+      )
+      # TRIP 1
+      start_time = Time.parse("2015-05-20T12:14:00+00:00")
+      end_time = start_time + 25 * 60 # 25 minutes
+      @trip_data_1 = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1,
+                                            name: "Ada",
+                                            phone_number: "412-432-7640"),
+        start_time: start_time.to_s,
+        end_time: end_time.to_s,
+        cost: 23.45,
+        rating: 3,
+      }
+      @trip_1 = RideShare::Trip.new(@trip_data_1)
+      @passenger.add_trip(@trip_1)
+
+      # TRIP 2
+      start_time = Time.parse("2015-05-21T12:14:00+00:00")
+      end_time = start_time + 5 * 60 # 5 minutes
+      @trip_data_2 = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1,
+                                            name: "Ada",
+                                            phone_number: "412-432-7640"),
+        start_time: start_time.to_s,
+        end_time: end_time.to_s,
+        cost: 5.00,
+        rating: 3,
+      }
+      @trip_2 = RideShare::Trip.new(@trip_data_2)
+      @passenger.add_trip(@trip_2)
+
+      # TRIP 3
+      start_time = Time.parse("2015-05-22T12:14:00+00:00")
+      end_time = start_time + 10 * 60 # 10 minutes
+      @trip_data_3 = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1,
+                                            name: "Ada",
+                                            phone_number: "412-432-7640"),
+        start_time: start_time.to_s,
+        end_time: end_time.to_s,
+        cost: 9.00,
+        rating: 3,
+      }
+      @trip_3 = RideShare::Trip.new(@trip_data_3)
+      @passenger.add_trip(@trip_3)
+    end
+
+    it "calculate total amount spent on all trips taken by one passenger" do
+      trip_prices_array = []
+      @passenger.trips.each do |trip|
+        trip_price = trip.cost
+        trip_prices_array << trip_price
+        # iterate through this passenger's trips, shovel into array below
+      end
+      amt_spent_total = @passenger.net_expenditures
+      expect(amt_spent_total).must_equal 37.45
+    end
+
+    it "calculate to total time spent on all trips taken by one passenger" do
     end
   end
 end
