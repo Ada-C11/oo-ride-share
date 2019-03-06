4303d1f4e900f29115ff2e3927eae180dfde943f
Ngoc Le <ngocle@outlook.com>
2019-03-06 15:14:41 -0800

    debugged

--- lib/driver.rb
+++ lib/driver.rb
@@ -21,7 +21,7 @@ module RideShare
                id: record[:id],
                name: record[:name],
                vin: record[:vin],
-               status: record[:status],
+               status: record[:status].to_sym,
              )
     end
   end
