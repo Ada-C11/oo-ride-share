ea4bc9cf92783d2c91d6738aad9f67dc83d95252
Jansen <v.jansen.martin@gmail.com>
2019-03-06 12:21:23 -0800

    Add @drivers instance variable to initialize method

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -1,16 +1,17 @@
-require 'csv'
-require 'time'
+require "csv"
+require "time"
 
-require_relative 'passenger'
-require_relative 'trip'
+require_relative "passenger"
+require_relative "trip"
 
 module RideShare
   class TripDispatcher
     attr_reader :drivers, :passengers, :trips
 
-    def initialize(directory: './support')
+    def initialize(directory: "./support")
       @passengers = Passenger.load_all(directory: directory)
       @trips = Trip.load_all(directory: directory)
+      @drivers = Driver.load_all(directory: directory)
       connect_trips
     end
 
