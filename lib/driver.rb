f64d3b599dd0a53668e806102f8c7a13486bc163
Jansen <v.jansen.martin@gmail.com>
2019-03-05 17:52:24 -0800

    Set default values for @status and @trips, add variables to attr_reader

--- lib/driver.rb
+++ lib/driver.rb
@@ -2,13 +2,19 @@ require_relative "csv_record"
 
 module RideShare
   class Driver < CsvRecord
-    attr_reader :vin, :status
+    attr_reader :name, :vin, :status, :trips
 
-    def initialize(id:, name:, vin:, status:, trips: nil)
+    def initialize(id:, name:, vin:, status: :AVAILABLE, trips: nil)
       super(id)
 
+      @name = name
       @vin = vin
       @status = status
+      @trips = trips || []
+
+      unless @vin.length == 17
+        raise ArgumentError, "Invalid VIN: #{vin}"
+      end
     end
 
     private
