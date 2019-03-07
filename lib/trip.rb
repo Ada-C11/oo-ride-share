80a877bfc6ec4fc6b3f0cf8f6415d549277d416c
qqdipps <sdipp007@gmail.com>
2019-03-07 15:36:37 -0800

    chagned rating of new trips to nil, added logic to initialize with rating nil.

--- lib/trip.rb
+++ lib/trip.rb
@@ -34,7 +34,7 @@ module RideShare
 
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
-      end
+      end if rating
 
       if driver
         @driver_id = driver.id
