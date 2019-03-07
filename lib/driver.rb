77b84e836c41198f1d5dcfb87b537d137c92787a
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-03-06 16:50:13 -0800

    fixed bug - driver status was a string

--- lib/driver.rb
+++ lib/driver.rb
@@ -27,7 +27,7 @@ module RideShare
                id: record[:id],
                name: record[:name],
                vin: record[:vin],
-               status: record[:vin],
+               status: record[:status].to_sym,
              )
     end
   end
