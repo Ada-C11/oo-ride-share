1735798055a84260eaf17402d1ab3ceaedfddece
Maria Wissler <wmcarmelina@gmail.com>
2019-03-06 16:15:08 -0800

    Add average_rating method

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,4 +1,4 @@
-require_relative "csv_record"
+require_relative 'csv_record'
 
 module RideShare
   class Driver < CsvRecord
@@ -12,24 +12,35 @@ module RideShare
       @status = status
       @trips = trips || []
 
-      unless @vin.length == 17
-        raise ArgumentError, "Invalid VIN: #{vin}"
-      end
+      raise ArgumentError, "Invalid VIN: #{vin}" unless @vin.length == 17
     end
 
     def add_trip(trip)
       @trips << trip
     end
 
+    def average_rating
+      if @trips.length == 0
+        return 0
+      else
+      average_rating = 0
+      @trips.each do |trip|
+        average_rating += trip.rating
+      end
+    end
+      average_rating /= @trips.length
+      return average_rating.to_f
+    end
+
     private
 
     def self.from_csv(record)
-      return new(
-               id: record[:id],
-               name: record[:name],
-               vin: record[:vin],
-               status: record[:status],
-             )
+      new(
+        id: record[:id],
+        name: record[:name],
+        vin: record[:vin],
+        status: record[:status]
+      )
     end
   end
 end
