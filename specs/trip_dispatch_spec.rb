fc7c3f0ced3cf744d5f551e4fc999d52ef7c36be
Jansen <v.jansen.martin@gmail.com>
2019-03-07 17:34:51 -0800

    Add test for trip_request method.  Passes.

--- specs/trip_dispatch_spec.rb
+++ specs/trip_dispatch_spec.rb
@@ -78,7 +78,6 @@ describe "TripDispatcher class" do
     end
   end
 
-  # TODO: un-skip for Wave 2
   describe "drivers" do
     describe "find_driver method" do
       before do
@@ -119,6 +118,16 @@ describe "TripDispatcher class" do
           expect(trip.driver.id).must_equal trip.driver_id
           expect(trip.driver.trips).must_include trip
         end
+      end
+    end
+    describe "trip_request method" do
+      before do
+        @dispatcher = build_test_dispatcher
+      end
+      it "creates a new instance of a trip" do
+        new_trip = @dispatcher.request_trip(1)
+
+        expect(new_trip).must_be_kind_of RideShare::Trip
       end
     end
   end
